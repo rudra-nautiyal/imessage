d75b8aee2a4c96b7ca8bf93533f3417306988540
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2026-06-28 02:54:46 +0530

    feat: add Clerk webhook handler to process user events and update database

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,14 +7,23 @@ import path from "path";
 
 import { clerkMiddleware } from "@clerk/express";
 
+import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
+
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
